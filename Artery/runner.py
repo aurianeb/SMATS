29e5d3a15b36e87c7da82fd34a63528ae859682a
aurianeb <auriane.blarre@eleves.enpc.fr>
2018-02-27 10:07:28 -0800

    First network design + traffic

---
+++ Artery/runner.py
@@ -0,0 +1,121 @@
+#!/usr/bin/env python
+"""
+@file    runner.py
+@author  Lena Kalleske
+@author  Daniel Krajzewicz
+@author  Michael Behrisch
+@author  Jakob Erdmann
+@date    2009-03-26
+@version $Id: runner.py 22608 2017-01-17 06:28:54Z behrisch $
+
+Tutorial for traffic light control via the TraCI interface.
+
+SUMO, Simulation of Urban MObility; see http://sumo.dlr.de/
+Copyright (C) 2009-2017 DLR/TS, Germany
+
+This file is part of SUMO.
+SUMO is free software; you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation; either version 3 of the License, or
+(at your option) any later version.
+"""
+from __future__ import absolute_import
+from __future__ import print_function
+
+import os
+import sys
+import optparse
+import subprocess
+import random
+
+# we need to import python modules from the $SUMO_HOME/tools directory
+try:
+    sys.path.append(os.path.join(os.path.dirname(
+        __file__), '..', 'sumo-0.30.0', "tools"))  # tutorial in tests
+    sys.path.append(os.path.join(os.environ.get("SUMO_HOME", os.path.join(
+        os.path.dirname(__file__), "..", "sumo-0.30.0")), "tools"))  # tutorial in docs
+    from sumolib import checkBinary
+except ImportError:
+    sys.exit(
+        "please declare environment variable 'SUMO_HOME' as the root directory of your sumo installation (it should contain folders 'bin', 'tools' and 'docs')")
+
+import traci
+
+
+def generate_routefile():
+    random.seed(42)  # make tests reproducible
+    N = 3600  # number of time steps
+    # demand per second from different directions
+    thresh = 10
+    with open("quickstart.rou.xml", "w") as routes:
+        print("""<routes>
+        <vType id="typeWE" accel="0.8" decel="4.5" sigma="0.5" length="5" minGap="2.5" maxSpeed="16.67" guiShape="passenger"/>
+        <vType id="typeNS" accel="0.8" decel="4.5" sigma="0.5" length="7" minGap="3" maxSpeed="25" guiShape="bus"/>
+
+        <route id="straight" edges="A12 A23 A34 A45 A56 A67 A79" />
+        <route id="right" edges="A12 m9o" />""", file=routes)
+        lastVeh = 0
+        vehNr = 0
+        for i in range(N):
+            if random.uniform(0, 1) < thresh:
+                print('    <vehicle id="straight_%i" type="typeWE" route="straight" depart="%i" />' % (
+                    vehNr, i), file=routes)
+                vehNr += 1
+                lastVeh = i
+            if random.uniform(0, 1) >= thresh:
+                print('    <vehicle id="right_%i" type="typeWE" route="right" depart="%i" />' % (
+                    vehNr, i), file=routes)
+                vehNr += 1
+                lastVeh = i
+        print("</routes>", file=routes)
+
+# The program looks like this
+#    <tlLogic id="0" type="static" programID="0" offset="0">
+# the locations of the tls are      NESW
+#        <phase duration="31" state="GrGr"/>
+#        <phase duration="6"  state="yryr"/>
+#        <phase duration="31" state="rGrG"/>
+#        <phase duration="6"  state="ryry"/>
+#    </tlLogic>
+
+
+def run():
+    """execute the TraCI control loop"""
+    step = 0
+    # we start with phase 2 where EW has green
+    traci.trafficlights.setPhase("0", 2)
+    while traci.simulation.getMinExpectedNumber() > 0:
+        traci.simulationStep()
+        # traci.lane.getTraveltime(":0_0_0")
+        step += 1
+    traci.close()
+    sys.stdout.flush()
+
+
+def get_options():
+    optParser = optparse.OptionParser()
+    optParser.add_option("--nogui", action="store_true",
+                         default=False, help="run the commandline version of sumo")
+    options, args = optParser.parse_args()
+    return options
+
+
+# this is the main entry point of this script
+if __name__ == "__main__":
+    options = get_options()
+
+    # this script has been called from the command line. It will start sumo as a
+    # server, then connect and run
+    if options.nogui:
+        sumoBinary = checkBinary('sumo')
+    else:
+        sumoBinary = checkBinary('sumo-gui')
+
+    # first, generate the route file for this simulation
+    generate_routefile()
+
+    # this is the normal way of using traci. sumo is started as a
+    # subprocess and then the python script connects and runs
+    traci.start([sumoBinary, "-c", "quickstart.sumocfg",
+                             "--tripinfo-output", "tripinfo.xml"])
+    run()
